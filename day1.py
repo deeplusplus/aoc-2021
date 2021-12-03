c87640b37cd544d9e4dce740871bf94e0fe56e2e
deeplusplus <r2dan2d2@gmail.com>
2021-12-03 17:55:12 -0500

    Hurray for not reading the directions thoroughly.  Didn't realize it was a sliding window at first.

--- day1.py
+++ day1.py
@@ -1,16 +1,27 @@
 
-
 def main():
     file = open('day1input.txt', 'r')
     increases = 0
-    previous_line = 9999999
+    previous_window = 999999
     lines = file.readlines()
 
-    for line in lines:
-        strip_line = int(line.strip())
-        if strip_line > previous_line:            
+    start = 0
+    end = 3
+
+    while end <= len(lines):
+        window = lines[start:end]
+        print(window)
+        num1 = int(window[0].strip())
+        num2 = int(window[1].strip())
+        num3 = int(window[2].strip())
+
+        window1 = num1 + num2 + num3
+
+        if window1 > previous_window:            
             increases += 1
-        previous_line = strip_line
+        previous_window = window1
+        start+=1
+        end+=1
 
     print(increases)
 
